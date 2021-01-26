7fe2d07e6b7d4e0d0716f8f2379f0ee25fa14b4a
Raghuram Krishnaswami <krish.raghuram@gmail.com>
2021-01-26 13:10:26 +0530

    Sort output by frequency (and by line string where different lines have same frequency)

--- DevNotes.md
+++ DevNotes.md
@@ -1,7 +1,16 @@
 # Notes.md
 
 ```
+# Commands to install package locally, for development
 rsync -azP ../SublimeFrequency /home/raghuram/.config/sublime-text-3/Packages
+while true; do date && rsync -azP ../SublimeFrequency /home/raghuram/.config/sublime-text-3/Packages && sleep 2; done
 
-while true; do date && rsync -azP ../SublimeFrequency /home/raghuram/.config/sublime-text-3/Packages && sleep 3; done
+# Tagging Releases
+git add -A
+git commit --amend
+git tag -d st3-0.1.0
+git tag -a st3-0.1.0 -m "Initial Release"
+git push -f origin master
+git push origin :refs/tags/st3-0.1.0
+git push origin --tags
 ```
--- SublimeFrequency.py
+++ SublimeFrequency.py
@@ -1,12 +1,7 @@
 """
-Sublime command to print frequency of lines (and words)
-
-TODO:
-* Add commands for separators like "," and " " and "\t"
-* Allow arbitrary separators
-* Sort JSON output by key (to make it easier for user to quickly lookup a word/line)
-* Sort JSON output by value (to easily see which words/lines are occuring most)
+Sublime Text 3 plugin that shows the frequency of each line in a document
 """
+
 
 import json
 import pprint
@@ -15,28 +10,34 @@ import collections
 import sublime
 import sublime_plugin
 
+
 class LineFrequencyCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         ### Get full text from current file
         lines = self.view.substr(sublime.Region(a=0,b=self.view.size())).splitlines()
         
         ### Do frequency counting of lines
-        freq = {unique_line:0 for unique_line in set(lines)}
+        freq = {}
         for line in lines:
-            freq[line] += 1
-        ### Sort by value
-        # freq = collections.OrderedDict(sorted(freq.items(), key=lambda i:i[1], reverse=True))
-        ### Sort by key
-        # freq = collections.OrderedDict(sorted(freq.items(), key=lambda i:i[0]))
-        content = json.dumps(freq, indent=4)
-        ### Sort by key inside json.dumps
-        # content = json.dumps(freq, indent=4, sort_keys=True)
+            freq.update({line:freq.get(line,0)+1})
 
-        ### Open a new window, and print frequency counts as csv/tsv
+        ### Sort by value, and by key for items having same value
+        freq = collections.OrderedDict(
+            sorted(
+                sorted(freq.items(), key=lambda i:i[0]),
+                key=lambda i:i[1], reverse=True
+            )
+        )
+        
+        ### Convert to JSON
+        content = json.dumps(freq, indent=4)
+
+        ### Open a new window and print content
         scratch = self.view.window().new_file()
         scratch.set_scratch(True)
         scratch.set_syntax_file('Packages/JavaScript/JSON.sublime-syntax')
         scratch.run_command('insert_content', {'content': content})
+
 
 class InsertContentCommand(sublime_plugin.TextCommand):
     def run(self, edit, content):
